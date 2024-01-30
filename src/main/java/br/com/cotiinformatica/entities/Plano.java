c6ffc373bca58f2b6b7d691635fcfa2729de6313
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-01-30 12:50:49 -0300

    feat: criação das entidades, connectionFactory e repositórios

---
+++ src/main/java/br/com/cotiinformatica/entities/Plano.java
@@ -0,0 +1,35 @@
+package br.com.cotiinformatica.entities;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Plano {
+	private UUID id;
+	private String nome;
+	private List<Cliente> clientes;
+
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
