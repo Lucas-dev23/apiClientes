86b1ee5f64d2ce6480c04094a6ed380dd9c6df43
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-02-06 12:51:06 -0300

    feat: cadastro, edição, exclusão e consulta de clientes da API.

--- src/main/java/br/com/cotiinformatica/entities/Plano.java
+++ src/main/java/br/com/cotiinformatica/entities/Plano.java
@@ -3,33 +3,11 @@ package br.com.cotiinformatica.entities;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.Data;
+
+@Data
 public class Plano {
 	private UUID id;
 	private String nome;
 	private List<Cliente> clientes;
-
-	public List<Cliente> getClientes() {
-		return clientes;
-	}
-
-	public void setClientes(List<Cliente> clientes) {
-		this.clientes = clientes;
-	}
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 }
